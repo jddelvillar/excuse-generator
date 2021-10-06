07b1cbb0108b5bf7ad200955897dc1542d7f7244
jddelvillar <delvillar2017.j@gmail.com>
2021-10-06 06:39:16 +0000

    second Excuse Generator

--- src/app.js
+++ src/app.js
@@ -8,4 +8,34 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let action = excuseGenerate();
+  console.log(action);
 };
+
+function excuseGenerate() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const array = document.querySelector("array");
+
+  return (
+    randomElement(who) +
+    " " +
+    randomElement(action) +
+    " " +
+    randomElement(what) +
+    " " +
+    randomElement(when)
+  );
+}
+function randomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
